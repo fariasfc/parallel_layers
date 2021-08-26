c161e9d10ed7f6735bdb9ca7507be42c18f070ff
Felipe Farias <felipefariax@gmail.com>
2021-08-26 09:14:15 -0300

    adjust scripts

--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -3,9 +3,9 @@ if [ $# -lt 1 ] ; then
     exit 1
 fi
 
-cd /home/fcf/projects/parallel_mlp/parallel_mlp
+cd /home/fcf/projects/parallel_mlps
 
 . ./.env
 # echo $WANDB_API_KEY
 # echo $WANDB_MODE
-CUDA_VISIBLE_DEVICES=0 WANDB_MODE=offline python src/main.py --multirun ${EXTRA_ARGS} training.project_name=$1
+CUDA_VISIBLE_DEVICES=0 WANDB_MODE=offline python parallel_mlps/main.py --multirun ${EXTRA_ARGS} training.project_name=$1
