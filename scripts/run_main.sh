9b1cd1b12b956e64e14fe6a0cb1177424e395e8e
Felipe Farias <felipefariax@gmail.com>
2021-11-18 00:32:58 -0300

    adjusting slurm and run_main script

--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -8,4 +8,6 @@ cd /home/fcf/projects/parallel_mlps
 . ./.env
 # echo $WANDB_API_KEY
 # echo $WANDB_MODE
-CUDA_VISIBLE_DEVICES=0 WANDB_MODE=offline python parallel_mlps/main.py --multirun ${EXTRA_ARGS} training.project_name=$1
+
+mkdir -p /tmp/outputs/$1/cache
+HYDRA_FULL_ERROR=1 WANDB_CACHE_DIR=/tmp/outputs/$1/cache CUDA_VISIBLE_DEVICES=0 WANDB_MODE=offline python parallel_mlps/main.py --multirun ${EXTRA_ARGS} training.project_name=$1
